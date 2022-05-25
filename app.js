f5d7d154dba39ad85911bc6ce1c89dcc4285d238
KienTruong123 <truongdinhkien321@gmail.com>
2022-05-26 00:11:20 +0700

    config

---
+++ app.js
@@ -0,0 +1,78 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var expressHandlebars = require('express-handlebars')
+var credentials = require('./credentials.js')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var flash = require('connect-flash');
+var session = require('express-session');
+var mongoose = require('mongoose')
+
+var app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+app.engine('handlebars', expressHandlebars.engine({
+  defaultLayout: 'main',
+  helpers: {
+      ifEquals: function(arg1, arg2, options) {
+          return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+      },
+  },
+}))
+app.use(flash());
+app.use(session({
+  secret: 'kiennguyentrong',
+  saveUninitialized: true,
+  resave: true
+}));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//database setup
+var opts = {
+}
+switch (app.get('env')) {
+    case 'development':
+        mongoose.connect(credentials.mongo.development.connectionString, opts)
+        break;
+    case 'production':
+        mongoose.connect(credentials.mongo.production.connectionString, opts)
+        break;
+    default:
+        throw new Error('Unknown excution environment'+ app.get('env'));
+}
+
+//route setup
+app.use(function (req, res, next) {
+  res.locals.flash = req.session.flash
+  delete req.session.flash
+  next()
+})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log(err)
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ credentials.js
@@ -0,0 +1,12 @@
+
+module.exports= {
+    cookieSecret: 'KienNguyenTrong',
+    mongo: {
+        development:{
+            connectionString:`mongodb+srv://Anonymous-Chat:zRSiHOGsBPwh56Tr@cluster0.exstx.mongodb.net/test`
+        },
+        production:{
+            connectionString:`mongodb+srv://Anonymous-Chat:zRSiHOGsBPwh56Tr@cluster0.exstx.mongodb.net/test`
+        }
+    }
+}
