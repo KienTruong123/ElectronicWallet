e920b8ddd2c38e512a4571baa2d851f889f3af79
NguyenT01 <transinguyen2001@gmail.com>
2022-05-29 23:13:20 +0700

    fix layout and add noti

--- public/js/main.js
+++ public/js/main.js
@@ -8,23 +8,56 @@ $('#slcard').change(function () {
     }
 });
 
-//----- USER WITHDRAW
+//----- USER WITHDRAW / DEPOSIT
 window.addEventListener("load", () => {
     let u_withdraw = document.querySelector("#money")
     let u_menu_fee_withdraw = document.querySelector("#u-menu-fee-withdraw")
     let u_menu_total = document.querySelector("#u-menu-total")
 
-    u_withdraw.addEventListener("change",() => {
-        u_menu_fee_withdraw.innerHTML = (u_withdraw.value * 0.05).toLocaleString() +"đ";
-        u_menu_total.innerHTML = (u_withdraw.value * 1.05).toLocaleString() +"đ";
+    u_withdraw.addEventListener("change", () => {
+        u_menu_fee_withdraw.innerHTML = parseFloat((u_withdraw.value * 0.05).toFixed(0)).toLocaleString() + "đ";
+        u_menu_total.innerHTML = parseFloat((u_withdraw.value * 1.05).toFixed(0)).toLocaleString() + "đ";
     });
 
 
+    //--- DEPOSIT PAGE
     let u_deposit_money = document.querySelector("#deposit_money")
     let u_deposit_total = document.querySelector("#u-deposit-total")
 
-    u_deposit_money.addEventListener("change",()=>{
-        u_deposit_total.innerHTML = (u_deposit_money.value * 1).toLocaleString() +"đ";
+    u_deposit_money.addEventListener("change", () => {
+        u_deposit_total.innerHTML = (u_deposit_money.value * 1).toLocaleString() + "đ";
     })
 
+    //--- TRANSFER PAGE
+    let t_reveiver_money = document.querySelector("#t_reveiver_money")
+    let t_reveiver_fee_withdraw = document.querySelector("#t_reveiver_fee_withdraw")
+    let t_reveiver_total = document.querySelector("#t_reveiver_total")
+    //let u_deposit_money = document.querySelector("#deposit_money")
+
+    t_reveiver_money.addEventListener("change", () => {
+        t_reveiver_fee_withdraw.innerHTML = parseFloat((t_reveiver_money.value * 0.05).toFixed(0)).toLocaleString() + "đ";
+        t_reveiver_total.innerHTML = parseFloat((t_reveiver_money.value * 1.05).toFixed(0)).toLocaleString() + "đ";
+    });
+
+    let button_send_otp_transfer = document.querySelector("#button_send_otp_transfer")
+
+    button_send_otp_transfer.addEventListener("click", e => {
+        e.preventDefault()
+        button_send_otp_transfer.disabled = true
+        setTimeout(() => {
+            button_send_otp_transfer.disabled = false
+            alert('btn clicked')
+        }, 60000)
+    })
 })
+
+
+//TODO: Change URL 
+function sendOTP(sessionID) {
+    $.ajax({
+        type: "POST",
+        url: "url",
+    });
+}
+
+
