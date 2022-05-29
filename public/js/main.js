42f15925106f728f281c3a11a944da0cf24e92ec
NguyenT01 <transinguyen2001@gmail.com>
2022-05-29 16:30:40 +0700

    withdraw_done

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,21 @@
 $('#slQuantity').hide();
-$('#slcard').change(function() {
+$('#slcard').change(function () {
     opt = $(this).val();
-    if (!(opt=="0")) {
+    if (!(opt == "0")) {
         $('#slQuantity').show();
-    }else{
+    } else {
         $('#slQuantity').hide();
     }
 });
+
+//----- USER WITHDRAW
+window.addEventListener("load", () => {
+    let u_withdraw = document.querySelector("#money")
+    let u_menu_fee_withdraw = document.querySelector("#u-menu-fee-withdraw")
+    let u_menu_total = document.querySelector("#u-menu-total")
+
+    u_withdraw.addEventListener("change",() => {
+        u_menu_fee_withdraw.innerHTML = (u_withdraw.value * 0.05).toLocaleString() +"đ";
+        u_menu_total.innerHTML = (u_withdraw.value * 1.05).toLocaleString() +"đ";
+    });
+})
